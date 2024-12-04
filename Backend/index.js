b4ad97634dc7e8e2c4753ad8e629cf9ca7b9f9d0
Abhishek Maurya <mauryaabhishek@gmail.com>
2024-12-04 15:03:53 +0530

    update

---
+++ Backend/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const userRouter = require('./router/userRouter');
+const subscriberRouter = require('./router/subscriberRouter');
+const utilRouter = require('./router/util');
+const cors = require('cors');
+
+const app = express();
+
+const port = 5000;
+
+app.use(express.json());
+
+// CORS configuration
+app.use(cors());
+
+// Logging incoming requests to /user route
+app.use('/user', (req, res, next) => {
+  console.log(`Incoming request to /user: ${req.method} ${req.url}`);
+  next();
+});
+
+// Define routers
+app.use('/user', userRouter);
+app.use('/util', utilRouter);
+app.use('/subscriber', subscriberRouter);
+
+// Serve static resources
+app.use(express.static('./static/resources'));
+
+// Base route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log('Server has started on port ' + port);
+});
